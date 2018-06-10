e832d16d278095d1e8be7de26e5cd70287d9cc88
Nathan Rempel <nsrempel@gmail.com>
2018-06-10 21:49:08 +1000

    Update to mainnet

--- src/scatter-client.jsx
+++ src/scatter-client.jsx
@@ -7,15 +7,15 @@ import { Button, Label } from 'react-bootstrap';
 
 const network = {
     blockchain:'eos',
-    host:'dev.cryptolions.io', // ( or null if endorsed chainId )
-    port:38888, // ( or null if defaulting to 80 )
-    chainId:"038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca", // Or null to fetch automatically ( takes longer )
+    host:'mainnet.genereos.io', // ( or null if endorsed chainId )
+    port:80, // ( or null if defaulting to 80 )
+    chainId:"aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", // Or null to fetch automatically ( takes longer )
 }
 
 const eosOptions = {
     broadcast: true,
     sign: true,
-    chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
+    chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
 }
 
 export function EosClient() {
